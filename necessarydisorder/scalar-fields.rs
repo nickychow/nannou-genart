1f10498406558ca27a69de7483c7bf8670bd2b53
Nick Chow <nicky.chow@gmail.com>
2022-04-14 15:53:17 +1000

    noticed real-time rendering wont work. make a scaled version

--- necessarydisorder/scalar-fields.rs
+++ necessarydisorder/scalar-fields.rs
@@ -1,5 +1,6 @@
 use nannou::prelude::*;
 
+// const MARGIN: f32 = 50.0;
 const NUM_FRAMES: u64 = 20;
 
 fn ease(p: f32, g: f32) -> f32 {
@@ -12,30 +13,36 @@ fn ease(p: f32, g: f32) -> f32 {
 }
 
 fn pixel_color(pos: &Vec2, time: f32) -> Rgb {
-    // let y = map_range(pos.y, -310.0, 310.0, 0.0, 720.0);
-    // let i = map_range(
-    //     (2.0 * PI * (time + scalar_field_offset(pos.x, pos.y))).sin(),
-    //     -1.0,
-    //     1.0,
-    //     0.0,
-    //     1.0,
-    // ) as f32;
     // let r = ease(i, 3.0);
 
-    let test = (2.0 * PI * (time + 0.05 * (pos.y))).sin();
+    let result = ease(
+        map_range(
+            (TAU * (time + (scalar_field_offset(pos.x, pos.y)))).sin(),
+            -1.0,
+            1.0,
+            0.0,
+            1.0,
+        ),
+        3.0,
+    );
 
-    rgb(test, test, test)
-    // rgb(r, r, r)
+    rgb(result, result, result)
 }
 
-// fn scalar_field_offset(x: f32, y: f32) -> f32 {
-//     0.05 * x + 0.05 * y
-// }
+fn scalar_field_offset(x: f32, y: f32) -> f32 {
+    let distance = (pt2(x, y)).distance(pt2(62.0, 62.0));
+    0.05 * distance
+}
 
 // This struct represents our "data state"
 // It should contain and model whatever we want to draw on screen
+struct Point {
+    pos: Vec2,
+    color: Rgba,
+}
+
 struct Model {
-    points: Vec<(Vec2, Rgb)>,
+    points: Vec<Point>,
 }
 
 // Builds the model
@@ -44,17 +51,17 @@ fn model(app: &App) -> Model {
     // also create it in the main function
     app.new_window().size(720, 720).build().unwrap();
 
-    let win = app.window_rect();
-    let win_p = win.pad(50.0);
+    // let win = app.window_rect();
+    // let win_p = win.pad(50.0);
 
-    let mut points = Vec::new();
+    let mut points: Vec<Point> = Vec::new();
 
-    for x in win_p.left() as i32..=win_p.right() as i32 {
-        for y in win_p.bottom() as i32..=win_p.top() as i32 {
-            let p = pt2(x as f32, y as f32);
-            let color = Rgb::new(0.0, 0.0, 0.0);
+    for x in 0..124 {
+        for y in 0..=124 {
+            let pos = pt2(x as f32, y as f32);
+            let color = Rgba::new(0.0, 0.0, 0.0, 1.0);
 
-            points.push((p, color));
+            points.push(Point { pos, color });
         }
     }
 
@@ -64,10 +71,10 @@ fn model(app: &App) -> Model {
 
 // Updates the model (note the mutable reference)
 fn update(app: &App, model: &mut Model, _update: Update) {
-    let time = ((app.elapsed_frames() - 1) % NUM_FRAMES) as f32 / NUM_FRAMES as f32;
-    // dbg!(time);
-    for (pos, color) in model.points.iter_mut() {
-        *color = pixel_color(pos, time);
+    let time = (app.elapsed_frames() - 1) as f32 / NUM_FRAMES as f32;
+
+    for point in model.points.iter_mut() {
+        *point.color = pixel_color(&point.pos, time);
     }
 }
 
@@ -75,16 +82,28 @@ fn update(app: &App, model: &mut Model, _update: Update) {
 fn view(app: &App, model: &Model, frame: Frame) {
     // We will use `draw` to draw on screen
     let draw = app.draw();
-
     let win = app.window_rect();
     let win_p = win.pad(50.0);
+    let offset = win_p.top_left();
+    let gdraw = draw.xy(offset).scale_y(-1.0).scale(5.0);
 
     // Let's first color the background
     draw.background().color(BLACK);
 
-    let points = model.points.to_owned();
+    // let points = model.points;
 
-    draw.polyline().weight(1.0).points_colored(points);
+    for point in model.points.iter() {
+        gdraw
+            .ellipse()
+            .xy(point.pos)
+            .w_h(1.0, 1.0)
+            .color(point.color);
+    }
+    // gdraw
+    //     .ellipse()
+    //     .xy(pt2(0.0, 10.0))
+    //     .w_h(1.0, 1.0)
+    //     .color(ORANGE);
 
     draw.rect()
         .xy(win_p.xy())
